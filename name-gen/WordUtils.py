fd0e5977948b2091c4f90903e226e158575ab174
Insality <insality@gmail.com>
2015-02-20 12:33:36 +0500

    fillind database

--- name-gen/WordUtils.py
+++ name-gen/WordUtils.py
@@ -1,5 +1,35 @@
 # coding: utf-8
 
 '''
-Модуль для редактиования слов (изменения падежей и окончаний)
+Модуль для получения и редактирования слов (изменения падежей и окончаний)
 '''
+
+import json
+import codecs
+import random
+
+DATADIR = "./data"
+
+def get_noun():
+	nouns = open_json("noun.json")["data"]
+	return nouns[random.randint(0, len(nouns) - 1)]["Word"]
+
+def get_adj():
+	adjs = open_json("adj.json")["data"]
+	return adjs[random.randint(0, len(adjs) - 1)]["Word"]
+
+def get_addon():
+	addons = open_json("addons.json")["data"]
+	return addons[random.randint(0, len(addons) - 1)]["Word"]
+
+def open_json(filename):
+	f = codecs.open(DATADIR + "/" + filename, 'r', encoding='utf8')
+	f_data = f.read()
+	return json.loads(f_data)
+	f.close()
+
+
+
+if __name__=="__main__":
+	for i in range(30):
+		print(get_adj() + " " + get_noun() + " " + get_addon())
