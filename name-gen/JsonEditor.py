3d8656fd64e16055fec55956628c84871d5d4940
Insality <insality@gmail.com>
2015-02-20 12:02:03 +0500

    implemented json editor, start filling databases

--- name-gen/JsonEditor.py
+++ name-gen/JsonEditor.py
@@ -8,6 +8,8 @@
 import os
 import json
 import Tags
+import codecs
+
 DATADIR = "./data"
 
 def json_editor():
@@ -18,10 +20,10 @@ def json_editor():
 	for index, filename in enumerate(jsons):
 		print("%s: %s" % (index, filename))
 
-	# index = raw_input("Enter file to open (index): ")
-	index = 0
+	index = int( input("Enter file to open (index): "))
+	# index = 0
 	print("Открываю %s" % jsons[index])
-	f = open(DATADIR + "/" + jsons[index])
+	f = codecs.open(DATADIR + "/" + jsons[index], 'r', encoding='utf8')
 	f_data = f.read()
 	json_data = json.loads(f_data)
 	f.close()
@@ -29,6 +31,7 @@ def json_editor():
 	print("Введите команду, help для справки")
 
 	command = input(">>> ")
+	# command = "add безысхощжность m Item Location"
 	args = command.split(' ')[1:]
 	command = command.split(' ')[0]
 
@@ -36,9 +39,21 @@ def json_editor():
 		if (command == "help"):
 			print_help()
 		elif (command == "add"):
-			pass
+			word = args[0]
+			genus = args[1]
+			tags = args[2:]
+			try:
+				cur_word = get_word_dict(word, genus, tags)
+				json_data["data"].append(cur_word)
+				print("Добавлено %s" % cur_word['Word'])
+			except AssertionError:
+				print("Неправильный формат слова")
+
 		elif (command == "list"):
-			pass
+			word_list = []
+			for word in json_data["data"]:
+				word_list.append(word["Word"])
+			print(", ".join(word_list))
 		elif (command == "rm"):
 			pass
 		else:
@@ -48,25 +63,39 @@ def json_editor():
 		args = command.split(' ')[1:]
 		command = command.split(' ')[0]
 
+	# saving:
+	f = codecs.open(DATADIR + "/" + jsons[index], 'w', encoding='utf8')
+	f.write(json.dumps(json_data, ensure_ascii=False))
+	f.close()
+
+
 def print_help():
 	print()
 	print("help - эта информация")
 	print()
-	print("add $word $genus $part $tags")
+	print("add $word $genus $tags")
 	print("word: русское слово, genus: род слова (m - мужской, f- женский)")
 	print("tags: тэги. Список возможных тэгов: %s " % ", ".join(Tags.Tags))
+	print("Пример: add Слово m Item Location")
 	print()
 	print("rm $word")
 	print("word: слово, которое необходимо удалить")
 	print()
 	print('list')
 	print('Выводит список слов')
+	print('exit')
+	print('Сохраняет и выходит из редактирования')
 
 def get_word_dict(word, genus, tags):
 	# asserts here
 	assert genus=='m' or genus=='f', "Genus is incorrect"
 	assert type(tags) is list, "Tags is not a list"
 	assert len(word)>0 and word.isalpha(), "Word is incorrect"
+
+	tags = list (map(lambda x: x.title(), tags))
+
+	for tag in tags:
+		assert tag in Tags.Tags, "Tags is incorrect"
 
 	word = {"Word": word.lower(), "Genus": genus, "Tags": tags}
 	return word
